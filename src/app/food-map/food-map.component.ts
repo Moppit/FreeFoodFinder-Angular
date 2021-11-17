766ae95a42748f059c38442aa0b670b8df57a526
Jett Crowson <jecr4062@colorado.edu>
2021-11-16 21:43:58 -0700

    Refactor. Redesign pins.

--- src/app/food-map/food-map.component.ts
+++ src/app/food-map/food-map.component.ts
@@ -13,60 +13,55 @@ import {MapInfoWindow, MapMarker} from "@angular/google-maps";
 export class FoodMapComponent implements OnInit {
 
 
-  public mapOptions: google.maps.MapOptions = {
-    center: {
-      lat: 40.007620, lng: -105.265649
-    },
-    restriction: {
-      latLngBounds: {
-        east: -105.235225,
-        north: 40.018363,
-        south: 39.998582,
-        west: -105.291545
-      }
-    }
-  }
-
+  public mapOptions: google.maps.MapOptions;
   public markerOptions: google.maps.MarkerOptions[] = [];
-
   public foodEvents: FoodEvent[];
-
   public infoWindows: google.maps.InfoWindow[] = [];
-
-  constructor(private databaseService: DatabaseService
-  ) {
-  }
-
-  ngOnInit()
-    :
-    void {
-    this.databaseService.getAllEvents().subscribe((res: GetAllEventsRes) => {
-      this.foodEvents = res.events;
-      res.events.forEach((event: FoodEvent) => {
-        this.markerOptions.push({
-          position: {
-            lat: event.locationID.latitude,
-            lng: event.locationID.longitude
-          } as google.maps.LatLngLiteral,
-          title: event.foodName,
-          clickable: true,
-        } as google.maps.MarkerOptions);
-          });
-      });
-    };
 
   @ViewChildren(MapInfoWindow) infoWindowsView: QueryList<MapInfoWindow>;
   @ViewChildren(MapMarker) markersView: QueryList<MapMarker>;
 
-  openInfoWindow(windowIndex: number) {
-    let curIdx = 0;
-    this.infoWindowsView.forEach((window: MapInfoWindow) => {
-      if (windowIndex === curIdx) {
-        window.open(this.markersView.get(windowIndex));
-        curIdx++;
-      } else {
-        curIdx++;
+  constructor(private databaseService: DatabaseService) {
+    this.mapOptions = {
+      center: {
+        lat: 40.007620, lng: -105.265649
+      },
+      restriction: {
+        latLngBounds: {
+          east: -105.235225,
+          north: 40.018363,
+          south: 39.998582,
+          west: -105.291545
+        }
       }
+    }
+  }
+
+  ngOnInit(): void {
+    this.databaseService.getAllEvents().subscribe((res: GetAllEventsRes) => {
+      this.foodEvents = res.events;
+      res.events.forEach((event: FoodEvent) => {
+
+        const options: google.maps.MarkerOptions = {
+          position: {
+            lat: event.locationID.latitude,
+            lng: event.locationID.longitude
+          },
+          title: event.foodName,
+          clickable: true,
+        };
+
+        this.markerOptions.push(options);
+
+      });
     });
+  };
+
+  openInfoWindow(windowIndex: number) {
+    this.infoWindowsView.get(windowIndex)!.open(this.markersView.get(windowIndex));
+  }
+
+  formatDate(date: string): string {
+    return new Date(date).toLocaleString();
   }
 }
